65b8bbee5248bee2831d7683f05f0c2a910c4fd1
Gabriel-p <gabrielperren@gmail.com>
2014-06-13 15:58:13 -0300

    data now split into blocks correctly

--- arXiv.py
+++ arXiv.py
@@ -2,15 +2,16 @@
 
 import urllib
 import os
+import re
 
 """
 Goes through the new submissions to present those more relevant to you.
 """
 
 # Get arXiv/astro-ph/new data.
-# file = urllib.urlopen("http://arxiv.org/list/astro-ph/new")
-# lines = [str(line) for line in file]
-# file.close()
+#file = urllib.urlopen("http://arxiv.org/list/astro-ph/new")
+#lines = [str(line) for line in file]
+#file.close()
 
 #f = urllib.urlopen("http://arxiv.org/list/astro-ph/new")
 #s = f.read()
@@ -19,22 +20,41 @@ Goes through the new submissions to present those more relevant to you.
 #ff = open("temp.del", "w")
 #ff.write(s)
 #ff.close()
-lines = open("temp.del", "r")
+lines = [str(line) for line in open("temp.del", "r")]
 
 # Abstracts are located between <dt> and </dd>
 
-# Find number of bugs left.
-ini_block, end_block = [], []
+# Store titles, links, authors and abstracts.
+articles, authors = [], []
 for index, line in enumerate(lines):
-	#print index, line[0:4]
+	# Get link.
 	if line[0:4] == '<dt>':
-		a = line.split(']</a>&nbsp;')
-		b = a[0].split('">[')
-		art_num = int(b[1])
-		ini_block.append(index)
-		raw_input()
-	if line == '</dd>':
-		end_block.append(index)
+		a = re.split('</a>&nbsp;  <span class="list-identifier"><a href="|" title="Abstract">', line)
+		link = 'arxiv.orx' + a[1]
+	# Get title.
+	if line[0:38] == '<span class="descriptor">Title:</span>':
+		a = re.split('Title:</span> |\n', line)
+		title = str(a[1])
+	# Get authors.
+	if line[0:28] == '<a href="/find/astro-ph/1/au':
+		a = re.split('all/0/1">|</a>', line)
+		authors.append(str(a[1]))
+	# Get abstract.
+	if line[0:3] == '<p>':
+		abstract = re.split('<p>', line)[1]
+		i = 1
+		while lines[index + i] != '</p>\n':
+			abstract = abstract + str(lines[index + i])
+			i += 1
+	# End of article.
+	if line == '</dd>\n':
+		# Store in single list.
+		article = [title, abstract, authors, link]
+		articles.append(article)
+		# Reset authors list.
+		authors = []
+
+print articles[17]
 
 	
 
