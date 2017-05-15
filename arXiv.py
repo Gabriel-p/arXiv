efdc89a2a720358d07324adf6223593d8484a6e6
Gabriel-p <gabrielperren@gmail.com>
2017-05-15 11:36:05 -0300

    add bayesian classifier, still work to do

--- arXiv.py
+++ arXiv.py
@@ -1,53 +1,19 @@
-#!/usr/bin/env python
+
+import os
+from os.path import join, realpath, dirname
+import re
+from collections import OrderedDict
+import numpy as np
 
 from bs4 import BeautifulSoup as BS
-from urllib.request import urlopen
-import re
+import requests
 import shlex
 import textwrap
+from textblob.classifiers import NaiveBayesClassifier
 
-
-def findWholeWord(w, string):
-    '''
-    Finds a single word or a sequence of words in the list 'string'.
-    Ignores upper/lowercasing. Returns True if 'w' was found and
-    False if it wasn't.
-    '''
-    pattern = re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE)
-    return True if pattern.search(string) else False
-
-
-def get_arxiv_data(categ):
-    '''
-    Downloads data from arXiv.
-    '''
-    html = urlopen("http://arxiv.org/list/" + categ + "/new")
-    soup = BS(html, 'lxml')
-
-    return soup
-
-
-def get_articles(soup):
-    '''
-    Splits articles into lists containing title, abstract, authors and link.
-    Article info is located between <dt> and </dd> tags.
-    '''
-    # Get links.
-    links = ['https://' + _.text.split()[0].replace(':', '.org/abs/') for _ in
-             soup.find_all(class_="list-identifier")]
-    # Get titles.
-    titles = [_.text.replace('\n', '').replace('Title: ', '') for _ in
-              soup.find_all(class_="list-title mathjax")]
-    # Get authors.
-    authors = [_.text.replace('\n', '').replace('Authors:', '')
-               for _ in soup.find_all(class_="list-authors")]
-    # Get abstract.
-    abstracts = [_.text.replace('\n', ' ') for _
-                 in soup.find_all('p', class_="mathjax")]
-
-    articles = list(zip(*[authors, titles, abstracts, links]))
-
-    return articles
+import nltk.data
+from nltk.corpus import stopwords
+import pickle
 
 
 def get_in_out():
@@ -77,57 +43,151 @@ def get_in_out():
     return in_k, ou_k, categs
 
 
-def get_rank(articles, in_k, ou_k):
+def get_arxiv_data(categ):
     '''
-    Ranks articles according to the in keywords and rejects those that
-    contain any out keywords.
+    Downloads data from arXiv.
     '''
-    art_rank = [0.] * len(articles)
+    # # html = requests.get("http://arxiv.org/list/" + categ + "/new")
+    # day, month, year = '1', '4', '2017'
+    # url = "https://arxiv.org/catchup?smonth=" + month + "&group=grp_&s" +\
+    #       "day=" + day + "&num=50&archive=astro-ph&method=with&syear=" + year
+    # html = requests.get(url)
+    # soup = BS(html.content, 'lxml')
+
+    # with open("temp", "wb") as f:
+    #     f.write(html.content)
+    with open("temp", "rb") as f:
+        soup = BS(f, 'lxml')
+
+    return soup
+
+
+def get_articles(soup):
+    '''
+    Splits articles into lists containing title, abstract, authors and link.
+    Article info is located between <dt> and </dd> tags.
+    '''
+    # Get links.
+    links = ['https://' + _.text.split()[0].replace(':', '.org/abs/') for _ in
+             soup.find_all(class_="list-identifier")]
+    # Get titles.
+    titles = [_.text.replace('\n', '').replace('Title: ', '') for _ in
+              soup.find_all(class_="list-title mathjax")]
+    # Get authors.
+    authors = [_.text.replace('\n', '').replace('Authors:', '')
+               for _ in soup.find_all(class_="list-authors")]
+    # Get abstract.
+    abstracts = [_.text.replace('\n', ' ') for _
+                 in soup.find_all('p', class_="mathjax")]
+
+    articles = list(zip(*[authors, titles, abstracts, links]))
+
+    return articles
+
+
+def findWholeWord(w, string):
+    '''
+    Finds a single word or a sequence of words in the list 'string'.
+    Ignores upper/lowercasing. Returns True if 'w' was found and
+    False if it wasn't.
+    '''
+    pattern = re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE)
+    return True if pattern.search(string) else False
+
+
+def keywProbability(N_in, N_out):
+    """
+    """
+    if (N_in + N_out) > 0:
+        K_p = max(0., (N_in - N_out) / (N_in + N_out))
+    else:
+        K_p = -1.
+
+    return K_p
+
+
+def get_Kprob(articles, in_k, ou_k):
+    '''
+    Obtains keyword base probabilities for each article, according to the
+    in/out keywords.
+    '''
+    art_K_prob = []
     # Loop through each article stored.
-    for art_indx, art in enumerate(articles):
+    for i, art in enumerate(articles):
+        N_in, N_out = 0., 0.
         # Search for rejected words.
-            for ou_indx, ou_keyw in enumerate(ou_k):
-                # Search titles, abstract and authors list.
-                for ata_indx, lst in enumerate(art[:3]):
-                    # If the keyword is in any list.
-                    if findWholeWord(ou_keyw, lst):
-                        # Assign a value based on its position
-                        # in the rejected keywords list (higher
-                        # values for earlier keywords)
-                        art_rank[art_indx] = art_rank[art_indx] - \
-                            ((3. - ata_indx) / (1. + ou_indx))
-            # Search for accepted keywords.
-            for in_indx, in_keyw in enumerate(in_k):
-                # Search titles, abstract and authors list.
-                for ata_indx, lst in enumerate(art[:3]):
-                    # If the keyword is in any list.
-                    if findWholeWord(in_keyw, lst):
-                        # Assign a value based on its position
-                        # in the accepted keywords list (higher
-                        # values for earlier keywords)
-                        art_rank[art_indx] = art_rank[art_indx] + \
-                            ((3. - ata_indx) / (1. + in_indx))
-    return art_rank
+        for ou_keyw in ou_k:
+            # Search titles, abstract and authors list.
+            for words in art[:3]:
+                # If the keyword is in any list.
+                if findWholeWord(ou_keyw, words):
+                    N_out += N_out + 1.
+        # Search for accepted keywords.
+        for in_keyw in in_k:
+            # Search titles, abstract and authors list.
+            for words in art[:3]:
+                # If the keyword is in any list.
+                if findWholeWord(in_keyw, words):
+                    N_in += N_in + 1.
+
+        art_K_prob.append(keywProbability(N_in, N_out))
+
+    return art_K_prob
 
 
-def sort_rev(art_rank, articles):
+def get_Bprob(mypath, articles):
+    '''
+    Obtains keyword base probabilities for each article, according to the
+    in/out keywords.
+    '''
+    stpwrds = stopwords.words("english")
+    try:
+        with open(join(mypath, "classifier.pkl"), "rb") as f:
+            cl = pickle.load(f)
+
+        art_B_prob = []
+        for art in articles:
+            title, abstract = str(art[1]), str(art[2])
+            # Remove stopwords plus some common words while maintaining
+            # abstract's order.
+            abstract = ' '.join(
+                [w for w in list(OrderedDict.fromkeys(abstract.split()))
+                 if w not in stpwrds])
+            art_B_prob.append(
+                cl.prob_classify(title + ' ' + abstract).prob("pos"))
+
+    except FileNotFoundError:
+        print("No previous classifier file found.\n")
+        art_B_prob, cl = [-1. for _ in articles], []
+
+    return art_B_prob, cl
+
+
+def sort_rev(articles, K_prob, B_prob):
     '''
     Sort articles according to rank and reverse list so larger values
     will be located first in the list.
     '''
-    # Sort according to rank.
-    art_sorted = [x for (y, x) in sorted(zip(art_rank, articles))]
+    art_rank = (np.array(K_prob) + np.array(B_prob)) / 2.
+    # Sort according to rank values.
+    art_zip = list(zip(art_rank, articles, K_prob, B_prob))
+    art_zip.sort()
     # Revert so larger values will be first.
-    art_s_rev = art_sorted[::-1]
+    art_s_rev = art_zip[::-1]
 
-    return art_s_rev
+    articles, K_prob, B_prob = list(zip(*art_s_rev))[1:]
+
+    return articles, K_prob, B_prob
 
 
-def main(N_art):
+def main():
     '''
     Query newly added articles to selected arXiv categories, rank them
     according to given keywords, and print out the ranked list.
     '''
+    mypath = realpath(join(os.getcwd(), dirname(__file__), 'input'))
+    nltk.data.path.append(mypath)
+
     # Read accepted/rejected keywords and categories from file.
     in_k, ou_k, categs = get_in_out()
 
@@ -141,32 +201,49 @@ def main(N_art):
         # Store titles, links, authors and abstracts into list.
         articles = articles + get_articles(soup)
 
-    # Obtain articles' ranks according to keywords.
-    art_rank = get_rank(articles, in_k, ou_k)
+    print("Obtaining probabilities.")
+    # Obtain articles' probabilities according to keywords.
+    K_prob = get_Kprob(articles, in_k, ou_k)
+    # Obtain articles' probabilities based on Bayesian analysis.
+    B_prob, cl = get_Bprob(mypath, articles)
+    # Sort articles.
+    articles, K_prob, B_prob = sort_rev(articles, K_prob, B_prob)
 
-    # Sort articles according to rank values.
-    art_s_rev = sort_rev(art_rank, articles)
-
-    print('\n\n')
-    for i in range(N_art):
+    train = []
+    for i, art in enumerate(articles):
         # Title
-        title = str(art_s_rev[i][1])
+        title = str(art[1])
         print(str(i + 1) + ')', textwrap.fill(title, 77))
         # Authors + arXiv link
-        authors = art_s_rev[i][0]
-        print(textwrap.fill(authors, 77), '(' + str(art_s_rev[i][3]) + ')\n')
+        authors = art[0]
+        print(textwrap.fill(authors, 77), '\n* ' + str(art[3]) + '\n')
         # Abstract
-        abstract = str(art_s_rev[i][2])
-        print(textwrap.fill(abstract, 80), '\n\n')
+        abstract = str(art[2])
+        print(textwrap.fill(abstract, 80))
+        print("\nK_p, B_p: {:.2f}, {:.2f}".format(K_prob[i], B_prob[i]))
+        pn = input("Pos (p) or Neg (n): ")
+        if pn == "p":
+            train.append([title + ' ' + abstract, 'pos'])
+        elif pn in ["n", "not", "N", "Not"]:
+            train.append([title + ' ' + abstract, 'neg'])
+        elif pn == "quit":
+            break
+
+    if train:
+        if cl:
+            # Update the classifier with the new training data
+            print("\nUpdating classifier.")
+            cl.update(train)
+        else:
+            # Generate classifier
+            print("\nGenerating classifier.")
+            cl = NaiveBayesClassifier(train)
+
+        with open(join(mypath, "classifier.pkl"), "wb") as f:
+            pickle.dump(cl, f)
+
+    print("Finished.")
 
 
 if __name__ == "__main__":
-
-    N_art = input('Number of articles to list (N): ')
-    try:
-        N_art = int(N_art)
-    except:
-        N_art = 10
-        print('Error. Using default value (N = {}).'.format(N_art))
-
-    main(N_art)
+    main()
